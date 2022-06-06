3623199e128b08dddae5a8748d3c51ab369266df
qiaofeng1227 <76487013@qq.com>
2022-06-06 16:37:03 +0800

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -1,3 +1,7 @@
-      - sudo rabbitmqctl delete_user guest
-      - sudo rabbitmqctl add_user $RABBITMQ_USER $RABBITMQ_PASSWORD
-      - sudo rabbitmqctl set_user_tags admin administrator
+#!/bin/bash
+# customrized cmd powered by Websoft9
+
+rabbitmqctl delete_user guest
+rabbitmqctl add_user $RABBITMQ_USER $RABBITMQ_PASSWORD
+rabbitmqctl set_user_tags admin administrator
+tail -f /dev/null
