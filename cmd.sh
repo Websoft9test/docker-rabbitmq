dd46e7198039a30b9ce6fea3853f503bbd33c366
qiaofeng1227 <76487013@qq.com>
2022-06-06 16:51:19 +0800

    Delete cmd.sh

--- cmd.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# customrized cmd powered by Websoft9
-
-rabbitmqctl delete_user guest
-rabbitmqctl add_user $RABBITMQ_USER $RABBITMQ_PASSWORD
-rabbitmqctl set_user_tags admin administrator
-tail -f /dev/null
